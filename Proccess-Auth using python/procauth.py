d977facb56aa84fd24ba20f6fa595d5f93a95cc2
akhileshkoti <akhileshkoti2001@gmail.com>
2021-01-12 23:20:26 +0530

    added input file

---
+++ Proccess-Auth using python/procauth.py
@@ -0,0 +1,9 @@
+blacklist=[]
+print('Enter the names of the processes that needs to be blacklisted (Enter to give and input \'q\' to quit')
+while(True):
+	n=input()
+	if(n=='q'):
+		break
+	blacklist.append(n)
+print('Your Processes are being blacklisted and are not be able to open')
+
